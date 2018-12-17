dea9e7389b80970618beeec9bdb15e0358ed9be8
tqzmn <uranourahaomotedayo@gmail.com>
2018-12-17 18:41:45 +0900

    Switch the debug mode.

--- poa_web/settings.py
+++ poa_web/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import django_heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
