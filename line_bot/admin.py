673ffddcc4bf989763f8c165a417d643b060e97e
tqzmn <uranourahaomotedayo@gmail.com>
2018-12-30 13:43:05 +0900

    Refactoring.

--- line_bot/admin.py
+++ line_bot/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import (
     Category,
     LineFriend,
+    LineSession,
 )
 
 admin.site.register(Category)
 admin.site.register(LineFriend)
+admin.site.register(LineSession)
